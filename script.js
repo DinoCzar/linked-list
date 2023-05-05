6ba1f3541a3c74354811ed4369c2d4caf922572c
DinoCzar <ryanb_booking@yahoo.com>
2023-05-05 14:57:54 -0700

    Add functions for append, prepend, removeFirst, and removeLast

---
+++ script.js
@@ -0,0 +1,115 @@
+// Factory function to create a new node
+function createNode(value) {
+	return {
+		value: value,
+		next: null,
+	};
+}
+
+// Factory function to create a new linked list
+function createLinkedList() {
+	return {
+		head: null,
+		tail: null,
+		length: 0,
+
+		// Add a new node to the beginning of the linked list
+		prepend(value) {
+			const node = createNode(value);
+
+			if (this.head === null) {
+				this.head = node;
+				this.tail = node;
+			} else {
+				node.next = this.head;
+				this.head = node;
+			}
+
+			this.length++;
+		},
+
+		// Add a new node to the end of the linked list
+		append(value) {
+			const node = createNode(value);
+
+			if (this.tail === null) {
+				this.head = node;
+				this.tail = node;
+			} else {
+				this.tail.next = node;
+				this.tail = node;
+			}
+
+			this.length++;
+		},
+
+		// Remove the first node from the linked list
+		removeFirst() {
+			if (this.head === null) {
+				return null;
+			}
+
+			const value = this.head.value;
+			this.head = this.head.next;
+			this.length--;
+
+			if (this.head === null) {
+				this.tail = null;
+			}
+
+			return value;
+		},
+
+		// Remove the last node from the linked list
+		removeLast() {
+			if (this.tail === null) {
+				return null;
+			}
+
+			let value = null;
+
+			if (this.head === this.tail) {
+				value = this.head.value;
+				this.head = null;
+				this.tail = null;
+			} else {
+				let current = this.head;
+				while (current.next !== this.tail) {
+					current = current.next;
+				}
+
+				value = this.tail.value;
+				current.next = null;
+				this.tail = current;
+			}
+
+			this.length--;
+			return value;
+		},
+
+		// Convert the linked list to an array
+		toArray() {
+			const array = [];
+
+			let current = this.head;
+			while (current !== null) {
+				array.push(current.value);
+				current = current.next;
+			}
+
+			return array;
+		},
+	};
+}
+
+const list = createLinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.prepend(0);
+console.log(list.toArray()); // Output: [0, 1, 2, 3]
+console.log(list.length); // Output: 4
+list.removeFirst();
+list.removeLast();
+console.log(list.toArray()); // Output: [1, 2]
+console.log(list.length); // Output: 2
